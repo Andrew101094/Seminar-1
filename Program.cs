f15decf0f3dbe27413998cd3d66e0b3771fb2ec2
Andrew <goscontract01@yandex.ru>
2023-07-10 21:19:25 +0300

    2

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, какое число большее.
+// a = 25, b = 7, c = 15 -> max = 25
+
+Console.WriteLine("Введите два числа: ");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+if (number1 > number2)
+{
+    int res = number1;
+    Console.WriteLine("Наибольшее число: " + res);
+}
+else
+{
+    int res = number2; 
+    Console.WriteLine("Наибольшее число: " + res);
+}
